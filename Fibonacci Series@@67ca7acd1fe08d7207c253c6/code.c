2eb84d6ad94653633a430ccbd8187cdc29edf381
Vansh Nagpal <nagpalv931@gmail.com>
2025-03-29 23:54:28 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,10 +1,9 @@
-int fibonacciSeries(int n){
-    int x=0,y=1,z;
-    while(n){
-        printf("%d ",x);
+void fibonacciSeries(int n){
+    int x = 0,y = 1,z;
+    while(n == 0){
+        printf("%d ",x)
         z = x + y;
         x = y;
         y = z;
-        n--;
     }
 }
