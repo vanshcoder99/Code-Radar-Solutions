e8659f59a3c6adaee7c07f665c0b8ce469ace799
Vansh Nagpal <nagpalv931@gmail.com>
2025-04-07 17:09:15 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -2,31 +2,33 @@
 
 int main() {
     int n;
-    scanf("%d", &n);
+    scanf("%d", &n);  // Read the number of elements
 
-    int arr[n];
-    for (int i = 0; i < n; i++) {  
-        scanf("%d", &arr[i]);
-    }
+    int arr[n], freq[n];  // Arrays to store elements and their frequencies
 
-    int maxFreq = 0;
-    int mostFrequent = arr[0];
     for (int i = 0; i < n; i++) {
-        int count = 1;  // Start counting the current element
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] == arr[j]) {
-                count++; // Increase frequency count
-            }
-        }
+        scanf("%d", &arr[i]);  // Read array elements
+        freq[i] = -1;  // Initialize all frequencies to -1
+    }
 
-        // Update most frequent element
-        if (count > maxFreq) {
-            maxFreq = count;
-            mostFrequent = arr[i];
+    for (int i = 0; i < n; i++) {
+        if (freq[i] == -1) {
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    freq[j] = 0;  // Mark duplicates
+                }
+            }
+            freq[i] = count;  // Store frequency for unique element
         }
     }
 
-    printf("%d", mostFrequent);
+    for (int i = 0; i < n; i++) {
+        if (freq[i] != 0) {
+            printf("%d %d\n", arr[i], freq[i]);  // Print unique elements and their frequencies
+        }
+    }
 
     return 0;
 }
